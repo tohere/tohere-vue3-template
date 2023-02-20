ae42e825065a4b9f66803b1a83d60dff27a691e4
tohere <guanglian.bai@xiaoi.com>
2023-02-20 18:46:04 +0800

    chore: add vitest

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,20 @@
 <template>
   <div>Home</div>
 
-  <h1>{{ count }}----{{ double }}</h1>
+  <h1>----{{ double }}</h1>
+  <h2 class="count">{{ count }}</h2>
+  <h2>msg: {{ msg }}</h2>
   <button @click="handleIncrement">增加</button>
 </template>
 
 <script lang="ts" setup>
 import { useUserStore } from "../../store/user";
 import { computed } from "vue";
+
+defineProps<{
+  msg?: string;
+}>();
+
 const userStore = useUserStore();
 const count = computed(() => userStore.count);
 const double = computed(() => userStore.doubleCount);
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -5,4 +6,8 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   envDir: "./env",
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 });
