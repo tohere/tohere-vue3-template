e617c3188f99f1cd9ea78be2821f9d05217a8d99
tohere <guanglian.bai@xiaoi.com>
2023-02-20 17:24:43 +0800

    chore: add pinia

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,18 @@
 <template>
   <div>Home</div>
+
+  <h1>{{ count }}----{{ double }}</h1>
+  <button @click="handleIncrement">增加</button>
 </template>
+
+<script lang="ts" setup>
+import { useUserStore } from "../../store/user";
+import { computed } from "vue";
+const userStore = useUserStore();
+const count = computed(() => userStore.count);
+const double = computed(() => userStore.doubleCount);
+
+function handleIncrement() {
+  userStore.increment();
+}
+</script>
